036adee42fee6eeab9f370291c7fbc81a6cea527
rocky <rock9247@naver.com>
2021-04-18 13:21:57 -0400

    add routing and redirecting

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About(){
+    return (
+    <div className="about__container">
+        <span>About this page: I build it beacuse I love movies.</span>
+        <span>Rocky_test</span>
+    </div>
+    );
+}
+
+export default About;
